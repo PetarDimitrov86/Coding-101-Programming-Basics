3f40aae360afeb96199a58e81e4a3f0b16d884ab
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-17 10:12:37 +0300

    Version 4

---
+++ First-Steps-In-Coding/SquareStarsVer4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class SquareStarsVer4
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine(new string('*', number));
+        for (int i = 0; i < number - 2; i++)
+        {
+            Console.Write("*");
+            for (int a = 0; a < number - 2; a++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("*");
+        }
+        Console.WriteLine(new string('*', number));
+    }
+}
