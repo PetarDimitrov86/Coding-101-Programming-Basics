7fee16ce4979407ed6f0afdbc4346070e56295b7
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-17 10:05:35 +0300

    Add files via upload

---
+++ First-Steps-In-Coding/RectangleArea.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class RectangleArea
+{
+    static void Main()
+    {
+        Console.Write("a = ");
+        var a = int.Parse(Console.ReadLine());              //изписването на допълнителен текст като а= понякога в джъдж може да не се приеме правилно
+        Console.Write("b = ");
+        var b = int.Parse(Console.ReadLine());
+        Console.WriteLine(a * b);
+    }
+}
