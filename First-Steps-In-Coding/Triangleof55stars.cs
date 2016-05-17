98b04e35699e50a3529b169a8122a94fe2859e88
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-17 10:06:46 +0300

    Add files via upload

---
+++ First-Steps-In-Coding/Triangleof55stars.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Triangleof55stars
+{
+    static void Main()
+    {
+        for (int i = 0; i < 11; ++i)
+        {
+            string asterisks = new string('*', i);          //стринга получава прогресивно нови стойности спрямо i до края на for цикъла
+            Console.WriteLine(asterisks);
+        }
+    }
+}
