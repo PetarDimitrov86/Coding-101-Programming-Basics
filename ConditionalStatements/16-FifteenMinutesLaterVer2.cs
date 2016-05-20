17d23b3708afaf2d371f7ad292f6ef7281cbcfca
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:52:08 +0300

    Add files via upload

---
+++ ConditionalStatements/16-FifteenMinutesLaterVer2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class FifteenMinuteLater
+{
+    static void Main(string[] args)
+    {
+        int chasove = int.Parse(Console.ReadLine());
+        int minuti = int.Parse(Console.ReadLine());
+
+        if (minuti <= 44)
+        {
+            Math.Abs(minuti += 15);
+            Console.WriteLine("{0}:{1}", chasove, minuti);
+        }
+        else if (chasove == 23 && minuti >= 45)
+        {
+            chasove = chasove - 23;
+            minuti += 15;
+            minuti -= 60;
+            Console.WriteLine("{0}:{1}", chasove, minuti.ToString().PadLeft(2, '0'));
+        }
+
+        else if (minuti >= 45)
+        {
+            chasove += 1;
+            minuti += 15;
+            minuti -= 60;
+            Console.WriteLine("{0}:{1}", chasove, minuti.ToString().PadLeft(2, '0'));
+        }
+    }
+}
