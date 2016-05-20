e69b6ba99f37022e11d0480da72a914038b874ce
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:32:05 +0300

    Add files via upload

---
+++ SimpleCalculations/14-MultipleCurrenciesConvVer3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class MultipleCurrenciesConvVer3
+{
+    static void Main()
+    {
+        decimal cash = decimal.Parse(Console.ReadLine());
+        string firstCurrency = Console.ReadLine();
+        string secondCurrency = Console.ReadLine();
+        var currencies = new Dictionary<string, decimal>()
+        {
+            {"BGN",1 },
+            {"USD",1.79549m },
+            {"EUR",1.95583m },
+            {"GBP",2.53405m },
+        };
+        decimal result = cash * (currencies[firstCurrency] / currencies[secondCurrency]);
+        Console.WriteLine(Math.Round(result, 2));
+    }
+}
