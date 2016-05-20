e5b9dd84f975f317875c1bf0fc5bcc5ae4422b9d
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:40:30 +0300

    Add files via upload

---
+++ ConditionalStatements/07-BonusPoints.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class BonusPoints
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double chetno;
+        double five;
+        double bonusPoints = 0;
+        if (a % 2 == 0)
+        {
+            chetno = 1;
+        }
+        else
+        {
+            chetno = 0;
+        }
+        if (a % 10 == 5)
+        {
+            five = 2;
+        }
+        else
+        {
+            five = 0;
+        }
+        if (a <= 100)
+        {
+            bonusPoints += 5;
+        }
+        else if (a > 100 && a <=1000)
+        {
+            bonusPoints = (0.2 * a);
+        }
+        else if (a > 1000)
+        {
+            bonusPoints = (0.1 * a);
+        }
+        Console.WriteLine(bonusPoints + (five + chetno));
+        Console.WriteLine((a+bonusPoints) + (five + chetno));
+    }
+}
