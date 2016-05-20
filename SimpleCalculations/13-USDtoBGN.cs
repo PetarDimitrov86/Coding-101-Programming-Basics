0f8a0390211215f4ef25671ca85aad3dc732cf05
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:26:35 +0300

    Add files via upload

---
+++ SimpleCalculations/13-USDtoBGN.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class USDtoBGN
+{
+    static void Main()
+    {
+        Console.Write("USD = ");
+        double usd = double.Parse(Console.ReadLine());
+        double bgn = usd * 1.79549;
+        Console.WriteLine("BGN = " + Math.Round(bgn, 2));
+    }
+}
