5c1465878553db4b6710cd7bd4e83a4d3c6bbfa7
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:33:10 +0300

    My Version

---
+++ SimpleCalculations/15-1000Days.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+class ThousandDays
+{
+    static void Main()
+    {
+        var dateString = Console.ReadLine();
+        var format = "dd-MM-yyyy";
+        var provider = new CultureInfo("bg-BG");
+        DateTime birthday = DateTime.ParseExact(dateString, format, provider);
+        DateTime future = birthday.AddDays(999);
+        Console.WriteLine(future.ToString(format));
+    }
+}
