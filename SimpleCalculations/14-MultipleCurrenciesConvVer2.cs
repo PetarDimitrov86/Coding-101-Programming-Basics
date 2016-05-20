e451ccb3987cc7aa9d8a8b89526836af4f694fb7
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:31:01 +0300

    Revenant's Version with switch-case

---
+++ SimpleCalculations/14-MultipleCurrenciesConvVer2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+class MultipleCurrenciesConvVer2
+{
+    static void Main()
+    {
+        var lev = 1.0;                                          
+        var dollar = 1.79549;
+        var euro = 1.95583;
+        var pound = 2.53405;
+        double value = double.Parse(Console.ReadLine());
+        var inputCurrency = Console.ReadLine();
+        var outputCurrency = Console.ReadLine();
+        switch (inputCurrency)                             
+        {
+            case "BGN":
+                break;
+            case "USD":
+                value = (value * dollar);
+                break;
+            case "EUR":
+                value = (value * euro);
+                break;
+            case "GBP":
+                value = (value * pound);
+                break;
+        }
+        switch (outputCurrency)                           
+        {
+            case "BGN":
+                value = value / lev;
+                break;
+            case "USD":
+                value = value / dollar;
+                break;
+            case "EUR":
+                value = value / euro;
+                break;
+            case "GBP":
+                value = value / pound;
+                break;
+        }
+        Console.WriteLine(Math.Round(value, 2));  
+    }
+}
