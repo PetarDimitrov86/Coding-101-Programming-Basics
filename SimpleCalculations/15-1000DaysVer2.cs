02b6b46bb13a6be9e5925d206fd5885cad470c48
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:34:36 +0300

    Version 2

---
+++ SimpleCalculations/15-1000DaysVer2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+class ThousandDays
+{
+    static void Main(string[] args)
+    {
+        string dateString = Console.ReadLine();
+        string format = "dd-MM-yyyy";
+        DateTime result = DateTime.ParseExact(dateString, format, CultureInfo.InvariantCulture);
+        CultureInfo provider = CultureInfo.InvariantCulture;
+        DateTime output = Convert.ToDateTime(result).AddDays(999);
+        Console.WriteLine(output.ToString("dd-MM-yyyy"));
+    }
+}
