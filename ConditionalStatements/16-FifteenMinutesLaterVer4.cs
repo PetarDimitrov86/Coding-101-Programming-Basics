037acdc23e54f151a3a72e3277b5fd1681a37849
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:54:32 +0300

    Version 4 using TimeSpan

---
+++ ConditionalStatements/16-FifteenMinutesLaterVer4.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class TimePlus15Minutes
+{
+    static void Main()
+    {
+        int hours = int.Parse(Console.ReadLine());
+        int min = int.Parse(Console.ReadLine());
+        TimeSpan tms = new TimeSpan(hours, min + 15, 0);
+        Console.WriteLine("{0:%h}:{0:mm}", tms);
+    }
+}
