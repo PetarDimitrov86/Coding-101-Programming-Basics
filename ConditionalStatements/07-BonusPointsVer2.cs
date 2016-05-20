438625e4e44dcef96ad7ff424dd9098c51fe03eb
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:41:36 +0300

    Version 2 - Short Code

---
+++ ConditionalStatements/07-BonusPointsVer2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class BonusPoints
+{
+    public static void Main(string[] args)
+    {
+        var num = float.Parse(Console.ReadLine());
+        float bonus = 5;
+        if (num > 1000)
+            bonus = num / 10;
+        else if (num > 100)
+            bonus = num / 5;
+        if (num % 2 == 0)
+            bonus = bonus + 1;
+        else if ((num % 5 == 0) && (!(num % 2 == 0)))
+            bonus = bonus + 2;
+        Console.WriteLine(bonus);
+        Console.WriteLine((bonus + num));
+    }
+}
