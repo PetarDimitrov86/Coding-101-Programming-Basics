c5e440bff2e73dfe55e09c6a722c05b05f1a59b7
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:23:44 +0300

    Add files via upload

---
+++ SimpleCalculations/11-CelsiusToFahrenheit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class CelsiusToFahrenheit
+{
+    static void Main()
+    {
+        Console.Write("Celsius = ");
+        double celsius = double.Parse(Console.ReadLine());
+        double fahrenheit = Math.Round(((celsius * 1.8) + 32), 3);
+        Console.WriteLine("Fahrenheit = " + fahrenheit);
+    }
+}
