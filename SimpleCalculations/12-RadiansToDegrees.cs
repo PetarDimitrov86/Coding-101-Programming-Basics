79c90a0e6ce6c4b5b1972409b2d430822b7ef4c0
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:25:22 +0300

    Add files via upload

---
+++ SimpleCalculations/12-RadiansToDegrees.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class RadiansToDegrees
+{
+    static void Main()
+    {
+        Console.Write("Enter radians = ");
+        double rad = double.Parse(Console.ReadLine());
+        double deg = ((180 / Math.PI) * rad);
+        Console.WriteLine("Degrees = " + Math.Round(deg, 0));
+    }
+}
