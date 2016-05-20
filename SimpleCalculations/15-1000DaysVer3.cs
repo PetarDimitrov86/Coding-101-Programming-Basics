346073c166ce60101bbff0653d39d61f6cd95480
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:35:51 +0300

    Version 3 - Shortest

---
+++ SimpleCalculations/15-1000DaysVer3.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class ThousandDays
+{
+    static void Main()
+    {
+        DateTime bitrhday = DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", null);
+        DateTime answer = bitrhday.AddDays(999);
+        Console.WriteLine("{0:dd-MM-yyyy}", answer);
+    }
+}
