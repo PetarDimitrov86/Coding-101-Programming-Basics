19b782d9722e1fee663b3b1ac1efd58df64eb1fe
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:21:10 +0300

    Add files via upload

---
+++ SimpleCalculations/07-CircleAreaPerimeter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class CircleAreaPerimeter
+{
+    static void Main()
+    {
+        Console.Write("Въведете r = ");
+        var r = double.Parse(Console.ReadLine());
+        var area = Math.PI * r * r;
+        var perimeter = 2 * Math.PI * r;
+        Console.WriteLine("Лицето е = " + area);
+        Console.WriteLine("Обиколката е = " + perimeter);
+    }
+}
