a2be8afe63924ee9fd805d3287a42269c26368bf
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:19:59 +0300

    Add files via upload

---
+++ SimpleCalculations/06-TrapezoidArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class TrapezoidArea
+{
+    static void Main()
+    {
+        Console.Write("a = ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("b = ");
+        double b = double.Parse(Console.ReadLine());
+        Console.Write("h = ");
+        double h = double.Parse(Console.ReadLine());
+        double area = (((a + b) * h) / 2);
+        Console.WriteLine("Trapezoid area = " + area);
+    }
+}
