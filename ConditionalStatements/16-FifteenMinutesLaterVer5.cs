ce68f299f6730632fb78c7c1d9534bbffc772436
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:55:27 +0300

    Add files via upload

---
+++ ConditionalStatements/16-FifteenMinutesLaterVer5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class TimePlusFifteenMinutes
+{
+    static void Main()
+    {
+        int hours = int.Parse(Console.ReadLine());
+        int minutes = int.Parse(Console.ReadLine());
+        minutes += 15;
+
+        if (minutes >= 60)
+        {
+            hours += 1;
+            minutes = minutes - 60;
+            if (hours > 23)
+            {
+                hours = 0;
+            }
+        }
+        Console.WriteLine("{0}:{1:00}", hours, minutes);
+    }
+}
