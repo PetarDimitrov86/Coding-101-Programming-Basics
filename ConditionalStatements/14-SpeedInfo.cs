7886d14bf6b640501aaf42342e96d70344c16cd6
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:48:31 +0300

    Add files via upload

---
+++ ConditionalStatements/14-SpeedInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class SpeedInfo
+{
+    static void Main(string[] args)
+    {
+        decimal speed = decimal.Parse(Console.ReadLine());
+        if (speed <= 10)
+        {
+            Console.WriteLine("slow");
+        }
+        else if (speed <= 50)
+        {
+            Console.WriteLine("average");
+        }
+        else if (speed <= 150)
+        {
+            Console.WriteLine("fast");
+        }
+        else if (speed <= 1000)
+        {
+            Console.WriteLine("ultra fast");
+        }
+        else
+        {
+            Console.WriteLine("extremely fast");
+        }
+    }
+}
