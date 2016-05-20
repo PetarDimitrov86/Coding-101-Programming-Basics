4641533ce0816714fa95e5aa4fe2f310e9632ac5
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-20 14:43:49 +0300

    Version 2 - Short Code

--- ConditionalStatements/08-SumSeconds.cs
+++ ConditionalStatements/08-SumSeconds.cs
@@ -3,28 +3,14 @@ class SumSeconds
 {
     static void Main()
     {
-        int vreme1 = int.Parse(Console.ReadLine());
-        int vreme2 = int.Parse(Console.ReadLine());
-        int vreme3 = int.Parse(Console.ReadLine());
-        int minuti;
-        int sekundi;
-        int sumarnoVreme = vreme1 + vreme2 + vreme3;
-        if (sumarnoVreme >= 60)
-        {
-            minuti = sumarnoVreme / 60;
-            sekundi = sumarnoVreme % 60;
-            if (sekundi >= 10)
-                Console.WriteLine("{0}:{1}", minuti, sekundi);
-            else
-                Console.WriteLine("{0}:0{1}", minuti, sekundi);
-        }
-        else if (sumarnoVreme >= 10)
-        {
-            Console.WriteLine("0:" + sumarnoVreme);
-        }
-        else
-        {
-            Console.WriteLine("0:0" + sumarnoVreme);
-        }
+        var firstTime = int.Parse(Console.ReadLine());
+        var secondTime = int.Parse(Console.ReadLine());
+        var thirdTime = int.Parse(Console.ReadLine());
+
+        var secondsSum = firstTime + secondTime + thirdTime;
+        var minutes = secondsSum / 60;
+        var seconds = secondsSum % 60;
+
+        Console.WriteLine("{0}:{1:00}", minutes, seconds);
     }
 }
