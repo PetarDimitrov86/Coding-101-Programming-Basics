c66a417d8d92b975e4f56f51ef197b462c2f47fc
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-21 11:27:23 +0300

    Add files via upload

---
+++ ComplexConditionalStatements/10-Cinema.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Cinema
+{
+    static void Main(string[] args)
+    {
+        string priceCategory = Console.ReadLine().ToLower();
+        int columns = int.Parse(Console.ReadLine());
+        int rows = int.Parse(Console.ReadLine());
+        if (priceCategory == "premiere")
+            Console.WriteLine("{0:f2} leva", columns * rows * 12);
+        else if (priceCategory == "normal")
+            Console.WriteLine("{0:f2} leva", columns * rows * 7.50);
+        else if (priceCategory == "discount")
+            Console.WriteLine("{0:f2} leva", columns * rows * 5);
+    }
+}
