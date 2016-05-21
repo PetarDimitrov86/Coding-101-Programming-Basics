8cc72ccd8ea08e25fc0f2298f21ee6356d7dad4a
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-21 11:48:53 +0300

    Add files via upload

---
+++ Loops/16-HalfSumElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class HalfSumElement
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int bigNum = int.MinValue;
+        int sumNum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int j = int.Parse(Console.ReadLine());
+            sumNum = sumNum + j;
+            if (bigNum < j)
+                bigNum = j;
+        }
+        sumNum = sumNum - bigNum;
+        if (bigNum == sumNum)
+            Console.WriteLine("Yes Sum = " + bigNum);
+        else
+            Console.WriteLine("No Diff = " + Math.Abs(sumNum - bigNum));
+    }
+}
