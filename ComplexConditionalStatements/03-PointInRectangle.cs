97deea3760f6cf7672ea3fdd8311523f728558a8
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-21 11:17:36 +0300

    Add files via upload

--- ComplexConditionalStatements/03-PointInRectangle.cs
+++ ComplexConditionalStatements/03-PointInRectangle.cs
@@ -1,34 +1,19 @@
 ﻿using System;
-class SmallShop
+class PointInRectangle
 {
     static void Main(string[] args)
     {
-        string product = Console.ReadLine();
-        string city = Console.ReadLine();
-        double quantity = double.Parse(Console.ReadLine());
-        if (city == "Sofia")
+        double x1 = double.Parse(Console.ReadLine());
+        double y1 = double.Parse(Console.ReadLine());
+        double x2 = double.Parse(Console.ReadLine());
+        double y2 = double.Parse(Console.ReadLine());
+        double x = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
+        if (x1 < x2 && y1 < y2)
         {
-            if (product == "coffee") Console.WriteLine(quantity * 0.50);
-            else if (product == "water") Console.WriteLine(quantity * 0.80);
-            else if (product == "beer") Console.WriteLine(quantity * 1.20);
-            else if (product == "sweets") Console.WriteLine(quantity * 1.45);
-            else if (product == "peanuts") Console.WriteLine(quantity * 1.60);
-        }
-        else if (city == "Plovdiv")
-        {
-            if (product == "coffee") Console.WriteLine(quantity * 0.40);
-            else if (product == "water") Console.WriteLine(quantity * 0.70);
-            else if (product == "beer") Console.WriteLine(quantity * 1.15);
-            else if (product == "sweets") Console.WriteLine(quantity * 1.30);
-            else if (product == "peanuts") Console.WriteLine(quantity * 1.50);
-        }
-        else if (city == "Varna")
-        {
-            if (product == "coffee") Console.WriteLine(quantity * 0.45);
-            else if (product == "water") Console.WriteLine(quantity * 0.70);
-            else if (product == "beer") Console.WriteLine(quantity * 1.10);
-            else if (product == "sweets") Console.WriteLine(quantity * 1.35);
-            else if (product == "peanuts") Console.WriteLine(quantity * 1.55);
+            if (x1 <= x && y1 <= y && x2 >= x && y2 >= y)
+                Console.WriteLine("Inside");
+            else Console.WriteLine("Outside");
         }
     }
 }
