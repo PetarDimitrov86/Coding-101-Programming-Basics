7f3f56b2b36a53194d9994a91c267a7f6555ce86
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-21 11:55:33 +0300

    Add files via upload

---
+++ Loops/18-EqualPairsVer2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class EqualPairs
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int sumNewPair = 0;
+        int sumOldPair = 0;
+        int maxDiff = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            sumOldPair = sumNewPair;
+            int f = int.Parse(Console.ReadLine());
+            int g = int.Parse(Console.ReadLine());
+            sumNewPair = f + g;
+            if (maxDiff < Math.Abs(sumOldPair - sumNewPair) && i >= 2)
+                maxDiff = Math.Abs(sumOldPair - sumNewPair);
+        }
+        if (sumNewPair == sumOldPair || sumOldPair == 0)
+            Console.WriteLine("Yes, value={0}", sumNewPair);
+        else
+            Console.WriteLine("No, maxdiff={0}", maxDiff);
+    }
+}
