93bb1f3c1ff7f02bc29ebe81a949d9dc16955669
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-21 11:14:18 +0300

    Add files via upload

---
+++ ComplexConditionalStatements/01-PersonalTitles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class PersonalTitles
+{
+    static void Main(string[] args)
+    {
+        double age = double.Parse(Console.ReadLine());
+        string gender = Console.ReadLine();
+        if (gender == "m")
+        {
+            if (age >= 16)
+                Console.WriteLine("Mr.");
+            else Console.WriteLine("Master");
+        }
+        if (gender == "f")
+        {
+            if (age >= 16)
+                Console.WriteLine("Ms.");
+            else Console.WriteLine("Miss");
+        }
+    }
+}
