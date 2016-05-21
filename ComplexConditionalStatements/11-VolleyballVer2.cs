4c5333cb60391f28596acdb6876dd6ddf3bf3022
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-21 11:29:22 +0300

    Add files via upload

---
+++ ComplexConditionalStatements/11-VolleyballVer2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Volleyball
+{
+    static void Main(string[] args)
+    {
+        string year = Console.ReadLine();
+        double holidays = double.Parse(Console.ReadLine());
+        double homeHolidays = double.Parse(Console.ReadLine());
+        double totalPlays = 0;
+        double weeks = 48;
+        double saturdaySofia = (weeks - homeHolidays) * 3 / 4;
+        double holidaysSofia = holidays * 2 / 3;
+        totalPlays = saturdaySofia + holidaysSofia + homeHolidays;
+
+        if (year == "leap")
+            totalPlays += (0.15 * totalPlays);
+        Console.WriteLine(Math.Floor(totalPlays));
+    }
+}
