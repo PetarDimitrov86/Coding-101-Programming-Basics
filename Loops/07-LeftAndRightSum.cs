2c3f4c2a1f7a9fb09dea902ceb58f7d42b66a833
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-21 11:42:19 +0300

    Add files via upload

---
+++ Loops/07-LeftAndRightSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class LeftAndRightSum
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            int k = int.Parse(Console.ReadLine());
+            sum1 = sum1 + k;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            int j = int.Parse(Console.ReadLine());
+            sum2 = sum2 + j;
+        }
+        if (Math.Abs(sum1 - sum2) == 0)
+            Console.WriteLine("Yes, sum = " + sum1);
+        else
+            Console.WriteLine("No, diff = " + Math.Abs(sum1 - sum2));
+    }
+}
