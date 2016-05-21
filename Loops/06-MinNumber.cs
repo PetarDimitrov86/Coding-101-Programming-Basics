c82cfd03f55f0655cc8de7978d9597bd04a7ed9f
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-21 11:39:46 +0300

    Add files via upload

---
+++ Loops/06-MinNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class MinNumber
+{
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        int minNumber = int.MaxValue;
+        for (int i = 1; i <= num; i++)
+        {
+            int k = int.Parse(Console.ReadLine());
+            if (k < minNumber)
+                minNumber = k;
+        }
+        Console.WriteLine(minNumber);
+    }
+}
