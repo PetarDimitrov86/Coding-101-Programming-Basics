f03ae28e16b8dc5d288bad5a8fba04ff03c78678
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-21 11:28:14 +0300

    Add files via upload

---
+++ ComplexConditionalStatements/11-Volleyball.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Volleyball
+{
+    static void Main(string[] args)
+    {
+        string year = Console.ReadLine();
+        int celebrations = int.Parse(Console.ReadLine());
+        int homeWeekend = int.Parse(Console.ReadLine());
+        double numberGames = 0.0d;
+        int weekends = 48 - homeWeekend;
+
+        if (year == "normal")
+            numberGames = ((0.75 * weekends) + ((2.0 / 3.0) * celebrations) + homeWeekend);
+        else if (year == "leap")
+        {
+            numberGames = ((0.75 * weekends) + ((2.0 / 3.0) * celebrations) + homeWeekend);
+            numberGames = 0.15 * numberGames + numberGames;
+        }
+        Console.WriteLine("{0}", Math.Truncate(numberGames));
+    }
+}
