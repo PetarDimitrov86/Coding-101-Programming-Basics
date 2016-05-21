6a6e2bb8504ff6f8f130a5308d35c0e31f07d2f7
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-21 11:38:06 +0300

    Add files via upload

---
+++ Loops/04-SumNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class SumNumbers
+{
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        int j = 0;
+        for (int i = 1; i <= num; i++)
+        {
+            int k = int.Parse(Console.ReadLine());
+            j = k + j;
+        }
+        Console.WriteLine(j);
+    }
+}
