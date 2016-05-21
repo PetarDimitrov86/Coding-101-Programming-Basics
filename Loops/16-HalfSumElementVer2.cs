58ac24d0a68447f4f87151699547abf3a3e315a0
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-21 11:49:52 +0300

    Add files via upload

---
+++ Loops/16-HalfSumElementVer2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+class HalfSumElementVer2
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+            sum += arr[i];
+        }
+        int sum1 = sum - arr.Max();
+        if (sum1 == arr.Max())
+        {
+            Console.WriteLine("Yes");
+            Console.WriteLine("Sum = {0}", sum1);
+        }
+        else if (sum1 < arr.Max())
+        {
+            Console.WriteLine("No");
+            Console.WriteLine("Diff = {0}", arr.Max() - sum1);
+        }
+        else if (sum1 > arr.Max())
+        {
+            Console.WriteLine("No");
+            Console.WriteLine("Diff = {0}", sum1 - arr.Max());
+        }
+    }
+}
