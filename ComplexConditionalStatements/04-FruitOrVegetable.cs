d62f1230b88bbd5ca5124f86168129d962858f39
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-21 11:18:34 +0300

    Add files via upload

---
+++ ComplexConditionalStatements/04-FruitOrVegetable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class FruitOrVegetable
+{
+    static void Main(string[] args)
+    {
+        string product = Console.ReadLine();
+        if (product == "banana" || product == "apple" || product == "kiwi" || product == "cherry" || product == "lemon" || product == "grapes")
+            Console.WriteLine("fruit");
+        else if (product == "tomato" || product == "cucumber" || product == "pepper" || product == "carrot")
+            Console.WriteLine("vegetable");
+        else
+            Console.WriteLine("unknown");
+    }
+}
