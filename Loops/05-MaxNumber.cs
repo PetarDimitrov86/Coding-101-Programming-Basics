324fdd94c44e8ee0069cf734ae9b07b6f8a7d934
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-21 11:38:59 +0300

    Add files via upload

---
+++ Loops/05-MaxNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class MaxNumber
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int maxNumber = int.MinValue;
+        for (int i = 0; i < n; i++)
+        {
+            int k = int.Parse(Console.ReadLine());
+            if (k >= maxNumber)
+                maxNumber = k;
+        }
+        Console.WriteLine(maxNumber);
+    }
+}
