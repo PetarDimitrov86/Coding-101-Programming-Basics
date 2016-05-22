306152ca54b7afc4f32e20d893433235600d8b39
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-22 19:02:39 +0300

    Add files via upload

---
+++ StringDictionariesLambdaLINQ/01-PrintStringLetters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class PrintStringLetters
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        for (int i = 0; i < text.Length; i++)
+        {
+            char letter = text[i];
+            Console.WriteLine("str[{0}] -> '{1}'", i, letter);
+        }
+    }
+}
