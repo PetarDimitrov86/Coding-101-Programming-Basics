f82781e7461cea8b6bf00cd0a08e7be1cf1c1e42
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-22 19:05:29 +0300

    Add files via upload

---
+++ ObjectsAndClasses/BigFactorial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Numerics;
+
+class BigFactorial
+{
+    static void Main(string[] args)
+    {
+        BigInteger num = BigInteger.Parse(Console.ReadLine());
+        BigInteger factorial = 1;
+        for (int i = 1; i <= num; i++)
+        {
+            factorial *= i;
+        }
+        Console.WriteLine(factorial);
+    }
+}
