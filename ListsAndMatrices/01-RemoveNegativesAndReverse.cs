73444835d01544441fa062aeef361ca778a3061a
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-22 18:59:49 +0300

    Add files via upload

---
+++ ListsAndMatrices/01-RemoveNegativesAndReverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> originalList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        for (int i = 0; i < originalList.Count; i++)
+        {
+            if (originalList[i] < 0)
+            {
+                originalList.RemoveAt(i);
+                i--;            
+            }
+        }
+        originalList.Reverse();
+        if (originalList.Count == 0)
+            Console.WriteLine("empty");
+        else 
+        Console.WriteLine(string.Join(" ", originalList));
+    }
+}
