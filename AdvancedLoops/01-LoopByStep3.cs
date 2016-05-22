ce5e75fc9bf38f8a958cac6e5332c9645fe05d21
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-22 18:46:56 +0300

    Add files via upload

---
+++ AdvancedLoops/01-LoopByStep3.cs
@@ -0,0 +1,12 @@
+using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i+=3)
+        {
+            Console.WriteLine(i);
+        }
+        }
+    }
